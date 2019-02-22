bf1c7029acc04120f5cd2492385a7f1f13f21ead
flamesdev <johannpayer123@gmail.com>
2019-02-21 19:01:09 -0500

    Bug fixes, optimizations, & added new countries

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 /*global $*/
-var array;
 $.getJSON("https://raw.githubusercontent.com/flamesdev/population-visualizer/master/data.json", function (json) {
     "use strict";
-	array = json;
-});
-
-window.onload = function () {
-    "use strict";
-    var max = 1418000000;
-    array.forEach(item => {
+    var max = json[0].Value;
+    json.forEach(item => {
         var p = document.createElement("p");
         p.innerHTML = item.Name + " - " + item.DisplayValue;
         var div = document.createElement("div");
         div.style.width = item.Value / max * 97 + "vw";
-        document.body.append(p); 
+        document.body.append(p);
         document.body.append(div);
     });
-};
+});
 
 const ItemData = class {
-    constructor(Name, Value, DisplayValue) {
+    constructor(Name, Value, DisplayValue, Color) {
         this.Name = Name;
         this.Value = Value;
         this.DisplayValue = DisplayValue;
