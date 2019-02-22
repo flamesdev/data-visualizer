33cd56d163da2b17b122d031664e982d12d6f7ed
flamesdev <johannpayer123@gmail.com>
2019-02-21 21:25:05 -0500

    Cleaned code & added a flag color

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $.getJSON("https://raw.githubusercontent.com/flamesdev/population-visualizer/mas
 		var p = document.createElement("p");
 		p.innerHTML = item.Name + " - " + item.DisplayValue;
 		var div = document.createElement("div");
-		div.style.width = item.Value / max * ((screen.width - 60) / screen.width) * 100 + "%";
+		div.style.width = item.Value / max * (screen.width / screen.width) * 100 + "%";
 		var color = item.Colors;
 		if (color != null) {
 			var colorText = "linear-gradient(to right,";
