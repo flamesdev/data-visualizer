a192bdfc2682deb7d8420c1315c2691b0d3e8ea0
flamesdev <johannpayer123@gmail.com>
2019-02-21 21:03:21 -0500

    Bug fixes & added more flag colors

--- script.js
+++ script.js
@@ -9,12 +9,21 @@ $.getJSON("https://raw.githubusercontent.com/flamesdev/population-visualizer/mas
 		div.style.width = item.Value / max * ((screen.width - 60) / screen.width) * 100 + "%";
 		var color = item.Colors;
 		if (color != null) {
-			var colorText;
-			if (color.length != 1)
-				for (var i = 0; i < color.length; i++)
-					colorText += "#" + color[i] + " " + i / color.length * 100 + "%";
-            else
+			var colorText = "linear-gradient(to right,";
+			if (color.length != 1) {
+				var lastPercentage;
+				for (var i = 0; i < color.length; i++) {
+					if (lastPercentage != null)
+						colorText += "#" + color[i] + " " + lastPercentage + ",";
+					lastPercentage = (i + 1) / color.length * 100 + "%";
+					colorText += "#" + color[i] + " " + lastPercentage;
+					if (i != color.length - 1)
+						colorText += ",";
+				}
+			} else
 				colorText = color[0];
+			colorText += ")";
+			console.log(colorText);
 			div.style.background = colorText;
 		}
 		document.body.append(p);
