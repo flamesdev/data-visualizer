885952646f333f875ee2ce44343b2876c19cd4a3
flamesdev <johannpayer123@gmail.com>
2019-03-03 21:15:26 -0500

    Made code dynamic
    Made the code dynamic so that it can adjust to different data sets.

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 /*global $*/
 $.getJSON("https://raw.githubusercontent.com/flamesdev/population-visualizer/master/data.json", function (json) {
 	"use strict";
-	var max = json[0].Value;
-	json.forEach(item => {
+	var screen = document.createElement("div");
+	screen.id = "screen1";
+	document.body.append(CreateScreen(json, screen, true, "flag icons", "flag icons/Blank.svg"));
+});
+
+function CreateScreen(data, screen, showIcon, iconDir, defaultIcon) {
+	var max = data[0].Value;
+	data.forEach(item => {
 		var p = document.createElement("p");
 		p.innerHTML = item.Name + " - " + NumberToText(parseInt(item.Value));
-        
+
 		var div = document.createElement("div");
-		div.style.width = item.Value / max * (screen.width / screen.width) * 100 + "%";
-        div.className = "bar";
-        
-        var titleDiv = document.createElement("div");
-        
-        var icon = document.createElement("img");
-        if (item.ShowFlag != null)
-            icon.src = "flag icons/" + item.Name + ".svg";
-        else
-            icon.src = "flag icons/Blank.svg";
-        
-        titleDiv.appendChild(icon);
-        titleDiv.appendChild(p);
-        
-		document.body.append(titleDiv);
-		document.body.append(div);
+		div.style.width = item.Value / max * 100 + "%";
+		div.className = "bar";
+
+		var titleDiv = document.createElement("div");
+
+		var icon = document.createElement("img");
+		if (showIcon)
+			if (item.HasIcon != null)
+				icon.src = iconDir + "/" + item.Name + ".svg";
+			else
+				icon.src = defaultIcon;
+
+		titleDiv.appendChild(icon);
+		titleDiv.appendChild(p);
+
+		screen.appendChild(titleDiv);
+		screen.appendChild(div);
 	});
-});
+	return screen;
+}
 
 function NumberToText(number) {
 	if (number >= 1000000000)
@@ -34,9 +42,9 @@ function NumberToText(number) {
 }
 
 const ItemData = class {
-	constructor(Name, Value, ShowFlag) {
+	constructor(Name, Value, HasIcon) {
 		this.Name = Name;
 		this.Value = Value;
-		this.ShowFlag = ShowFlag;
+		this.HasIcon = HasIcon;
 	}
 }
