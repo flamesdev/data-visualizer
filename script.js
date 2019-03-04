e0e37c5299a75916827fe74819a0395a3ec6fd64
flamesdev <johannpayer123@gmail.com>
2019-03-03 23:00:36 -0500

    Added new datasets

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
-var screenID = 0;
+var screenID = null;
+
+var titleElement;
+window.onload = function () {
+	titleElement = document.getElementById("title");
+};
 
 var json;
 $.getJSON("https://raw.githubusercontent.com/flamesdev/population-visualizer/master/data.json", function (data) {
 	"use strict";
 	json = data;
-	document.body.append(CreateScreen(0, true, true, "flag icons", "Blank"));
+	CreateScreen(0, true, "flag icons", "Blank");
+	CreateScreen(1, false, null, null);
+	CreateScreen(2, false, null, null);
+	UpdateScreen(0);
+	document.body.style.visibility = "visible";
 });
 
-function CreateScreen(id, show, showIcon, iconDir, defaultIcon) {
+var touchStart;
+var touchX;
+var touchY;
+window.addEventListener("touchstart", function (e) {
+	touchStart = new Date();
+	touchX = e.clientX;
+	touchY = e.clientY;
+});
+
+window.addEventListener("touchend", function (e) {
+	if (new Date() - touchStart >= 3000 &&
+		Math.abs(e.clientX - touchX) / screen.width <= 0.1 &&
+		Math.abs(e.clientY - touchY) / screen.height <= 0.1)
+		NextScreen();
+});
+
+document.addEventListener("keydown", (event) => {
+	if (event.keyCode === 37)
+		if (screenID - 1 >= 0)
+			UpdateScreen(screenID - 1);
+		else
+			UpdateScreen(2);
+	if (event.keyCode === 39)
+		NextScreen();
+});
+
+function NextScreen() {
+	if (screenID + 1 <= 2)
+		UpdateScreen(screenID + 1);
+	else
+		UpdateScreen(0);
+}
+
+function CreateScreen(id, showIcon, iconDir, defaultIcon) {
 	"use strict";
 	iconDir += "/";
 	var screen = document.createElement("div");
 	screen.id = "screen" + id;
-	var data = json[id];
+	var data = json[id].Dataset;
 	var max = data[0].Value;
 	data.forEach(item => {
 		var p = document.createElement("p");
@@ -36,14 +78,23 @@ function CreateScreen(id, show, showIcon, iconDir, defaultIcon) {
 
 		screen.appendChild(titleDiv);
 		screen.appendChild(div);
+		screen.style.visibility = "hidden";
+		screen.style.display = "none";
 	});
-	if (show)
-		screen.style.visibility = "show";
-	else {
+	document.body.append(screen);
+}
+
+function UpdateScreen(newValue) {
+	if (screenID != null) {
+		var screen = document.getElementById("screen" + screenID);
 		screen.style.visibility = "hidden";
 		screen.style.display = "none";
 	}
-	return screen;
+	screenID = newValue;
+	title.innerHTML = json[screenID].Name;
+	var screen = document.getElementById("screen" + screenID);
+	screen.style.visibility = "visible";
+	screen.style.display = "block";
 }
 
 function NumberToText(number) {
