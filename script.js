4f4d3482654b96cbd1bc1d2615437dd4e1c4b987
flamesdev <johannpayer123@gmail.com>
2020-12-15 20:13:40 -0500

    Cleaned code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* global data title document window */
 let screenId = null;
 const maxIndex = data.datasets.length - 1;
 
@@ -15,67 +16,63 @@ function setScreen(newValue) {
   screen.style.display = 'block';
 }
 
-function truncateToDecimals(number, decimal) {
-  const pow = 10 ** decimal;
-  return Math.trunc(number * pow) / pow;
-}
-
-function numberToText(number, beginRound, decimal) {
-  const suffixes = [ '', 'K', 'M', 'B', 'T' ];
-  const id = Math.trunc((number.toString().length - 1) / 3);
-  number /= 10 ** (id * 3);
-  
-  return beginRound !== null && id - 1 >= beginRound ? truncateToDecimals(number, decimal) + suffixes[id] : Math.trunc(number) + suffixes[id];
-}
-
-function nextScreen() {
+function goToNextScreen() {
   setScreen(screenId < maxIndex ? screenId + 1 : 0);
 }
 
-function populateScreen(id) {
-  const screen = document.createElement('div');
-  screen.id = `screen${id}`;
-  screen.className = 'screen';
-
-  const dataset = data.datasets[id];
-  const datasetData = dataset.data;
-  const iconset = data.iconsets[dataset.iconsetId];
-  const max = datasetData[0].value;
-  datasetData.forEach((x) => {
-    const p = document.createElement('p');
-    p.innerHTML = `${x.name} - ${numberToText(parseInt(x.value * dataset.scale, 10), dataset.beginRound, dataset.toDecimal)}`;
-
-    const div = document.createElement('div');
-    div.style.width = `${x.value / max * 100}%`;
-    div.className = 'bar';
-
-    const span = document.createElement('span');
-
-    const icon = document.createElement('img');
-    if (iconset) {
-      icon.src = `icons/${iconset.directory}/${iconset.items.includes(x.name) ? x.name.toLowerCase().replace(/ /g, '_') : 'placeholder'}.svg`;
+window.addEventListener('load', () => {
+  function numberToText(number, beginRound, decimal) {
+    function truncateToDecimals(number2, decimal2) {
+      const pow = 10 ** decimal2;
+      return Math.trunc(number2 * pow) / pow;
     }
 
-    span.appendChild(icon);
-    span.appendChild(p);
+    const suffixes = [ '', 'K', 'M', 'B', 'T' ];
+    const id = Math.trunc((number.toString().length - 1) / 3);
+    const adjustedNumber = number / (10 ** (id * 3));
 
-    screen.appendChild(span);
-    screen.appendChild(div);
-    screen.style.visibility = 'hidden';
-    screen.style.display = 'none';
-  });
+    return (beginRound && id - 1 >= beginRound ? truncateToDecimals(adjustedNumber, decimal) : Math.trunc(adjustedNumber)) + suffixes[id];
+  }
 
-  const button = document.createElement('button');
-  button.innerHTML = 'Next screen';
-  button.addEventListener('click', nextScreen);
-
-  screen.appendChild(button);
-  document.body.append(screen);
-}
-
-window.addEventListener('load', () => {
   for (let i = 0; i < data.datasets.length; i++) {
-    populateScreen(i);
+    const screen = document.createElement('div');
+    screen.id = `screen${i}`;
+    screen.className = 'screen';
+
+    const dataset = data.datasets[i];
+    const datasetData = dataset.data;
+    const iconset = data.iconsets[dataset.iconsetId];
+    const max = datasetData[0].value;
+    datasetData.forEach((x) => {
+      const p = document.createElement('p');
+      p.innerHTML = `${x.name} - ${numberToText(parseInt(x.value * dataset.scale, 10), dataset.beginRound, dataset.toDecimal)}`;
+
+      const div = document.createElement('div');
+      div.style.width = `${x.value / max * 100}%`;
+      div.className = 'bar';
+
+      const span = document.createElement('span');
+
+      const icon = document.createElement('img');
+      if (iconset) {
+        icon.src = `icons/${iconset.directory}/${iconset.items.includes(x.name) ? x.name.toLowerCase().replace(/ /g, '_') : 'placeholder'}.svg`;
+      }
+
+      span.appendChild(icon);
+      span.appendChild(p);
+
+      screen.appendChild(span);
+      screen.appendChild(div);
+      screen.style.visibility = 'hidden';
+      screen.style.display = 'none';
+    });
+
+    const button = document.createElement('button');
+    button.innerHTML = 'Next screen';
+    button.addEventListener('click', goToNextScreen);
+
+    screen.appendChild(button);
+    document.body.append(screen);
   }
 
   setScreen(0);
@@ -87,6 +84,6 @@ document.addEventListener('keydown', (event) => {
   if (key === 'ArrowLeft') {
     setScreen(screenId <= 0 ? maxIndex : screenId - 1);
   } else if (key === 'ArrowRight') {
-    nextScreen();
+    goToNextScreen();
   }
 });
